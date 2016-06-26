59272b24c56473058857fabbc44ed059944c68e4
Matt Johnson <mrj@actuallymrj.com>
2016-06-25 23:15:23 -0500

    Update Calendar test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,28 +3,12 @@ import XCTest
 import NSDateExtensions
 
 class Tests: XCTestCase {
-//    override func setUp() {
-//        super.setUp()
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//    
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
-//    
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        XCTAssert(true, "Pass")
-//    }
-//    
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measureBlock() {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-    func testDecompositions() {
-        
+    func testCurrentCalendar() {
+        //Verify not nil - needed in Swift?
+        XCTAssertNotNil(NSDate.currentCalendar)
+        //Verifies that API doesn't change
+        XCTAssert(NSDate.currentCalendar is NSCalendar)
+        //Naive implementation test
+        XCTAssertEqual(NSDate.currentCalendar, NSCalendar.autoupdatingCurrentCalendar())
     }
 }
